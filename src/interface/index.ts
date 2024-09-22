4497a486fe21d159cad9d566497e6f15bf555b16
anasantos-dev <acws_ss@hotmail.com>
2024-09-22 00:08:16 -0300

    feat:

---
+++ src/interface/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import {configureDependencies} from '../infrastructure/utils/config';
+
+export const app = express()
+app.use(express.json())
+
+
+const {movieController} = configureDependencies();
+app.post ('/movies', (req, res) => movieController.create(req,res)); 
+
+app.get('/movies', (req, res) => movieController.listAll(req, res));
+
+app.get('/movies/:id', (req, res) => movieController.getMovieById(req, res));
+
+app.delete('/movies/:id', (req, res) => movieController.deleteMovieById(req, res));
+
+
+if (require.main === module) {
+    const PORT = 3333;
+    app.listen(PORT, ()=>{
+        console.log(`Servidor rodando na porta ${PORT}`);
+    })
+}
